140b4265691622fa619e3a501a4ca5b6029bab1b
damr <damr18@gmail.com>
2020-12-17 08:00:43 -0600

    connect to db and validator

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User')
 
 // @route  POST api/users
 // @desc  register a user
 // @access  Public
 
-router.post('/', (req,res) => {
-    res.send('Register a user');
+router.post('/',[
+
+    check('name','name is required').not().isEmpty(),
+    check('email','please include a valid email').isEmail(),
+    check('password','Please enter a password with 6 or more characters').isLength({min:6})
+
+], (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    res.send('passed');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const connetDB = require('./config/db')
 
 const app = express();
+
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({extended:false}));
 
 
 app.get('/', (req,res) => res.json({ msg: 'welcome to the contactkeeper api'}));
