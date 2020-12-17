6f6b56b417e60a2eb2cc05a5d5243739f7d9211a
damr <damr18@gmail.com>
2020-12-17 07:12:14 -0600

    Express initial setup with initial Routes

---
+++ routes/contacts.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', (req,res) => {
+    res.send('Get all contacts');
+});
+router.post('/', (req,res) => {
+    res.send('add contact');
+});
+router.put('/:id', (req,res) => {
+    res.send('update contact');
+});
+router.delete('/:id', (req,res) => {
+    res.send('delete contact');
+});
+
+module.exports = router;
