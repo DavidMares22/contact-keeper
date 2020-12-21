2973015fefa8b136c90c361c31bc8d13eb3145d3
damr <damr18@gmail.com>
2020-12-21 10:14:02 -0600

    react contacts, nav, context, roter

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,67 @@
+import React, {useReducer} from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer'
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts :[
+            {
+                id:1,
+                name: 'Jill Johnson',
+                email:'jill@gmail.com',
+                phone: '111-111-111',
+                type: 'personal'
+            },
+            {
+                id:2,
+                name: 'Sara Watson',
+                email:'sara@gmail.com',
+                phone: '333-333-121',
+                type: 'personal'
+            },
+            {
+                id:3,
+                name: 'Harry White',
+                email:'harry@gmail.com',
+                phone: '333-111-111',
+                type: 'professional'
+            },
+        ]
+    }
+
+    const [state,dispatch] = useReducer(contactReducer,initialState)
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Set current contact
+    
+    // Clear current contact
+    
+    // Update contact
+
+
+    return (
+        <ContactContext.Provider
+        value={
+           { contacts: state.contacts}
+        }
+        >
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+
+export default ContactState
