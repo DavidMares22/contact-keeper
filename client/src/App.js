7b957140dc514b93d3e701b4d534073cea6bf531
damr <damr18@gmail.com>
2020-12-26 13:01:36 -0600

    PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,15 @@ import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 
-import Register from "./components/auth/Register"
-import Login from "./components/auth/Login"
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import setAuthToken from "./utils/setAuthToken"
+import setAuthToken from "./utils/setAuthToken";
 
+import PrivateRoute from "./components/routing/PrivateRoute"
 
-if(localStorage.token){
-  setAuthToken(localStorage.token)
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
 function App() {
@@ -26,20 +27,20 @@ function App() {
     <AuthState>
       <ContactState>
         <AlertState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Alerts />
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertState>
       </ContactState>
     </AuthState>
